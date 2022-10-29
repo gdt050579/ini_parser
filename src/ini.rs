91819ca152d63731681897b26250e27f0aa55092
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-10-29 09:14:24 +0300

    Store section names

--- src/ini.rs
+++ src/ini.rs
@@ -6,7 +6,9 @@ struct Section {
     name: String,
     items: HashMap<u64, KeyValue>,
 }
-pub struct Ini {}
+pub struct Ini {
+    sections: HashMap<u64, Section>,
+}
 
 #[derive(PartialEq, Debug)]
 enum Status {
@@ -288,12 +290,44 @@ const CHAR_TYPE: [CharType; 256] = [
     CharType::Word,
 ];
 
+const LOWER_CASE_TABLE: [u8; 256] = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+    26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+    50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 97, 98, 99, 100, 101, 102, 103,
+    104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
+    91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
+    112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130,
+    131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149,
+    150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168,
+    169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187,
+    188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206,
+    207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225,
+    226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244,
+    245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
+];
+fn compute_string_hash(buf: &[u8]) -> u64 {
+    // use FNV algorithm ==> https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+    if buf.len() == 0 {
+        return 0;
+    }
+    let mut hash = 0xcbf29ce484222325u64;
+    let mut idx = 0usize;
+    while idx < buf.len() {
+        hash = hash ^ (LOWER_CASE_TABLE[buf[idx] as usize] as u64);
+        //hash = hash * 0x00000100000001B3u64;
+        hash = hash.wrapping_mul(0x00000100000001B3u64);
+        idx += 1;
+    }
+    return hash;
+}
 struct ParserObject<'a> {
     status: Status,
     ini: &'a mut Ini,
     pos: usize,
     buf: &'a [u8],
     text: &'a str,
+    current_section: Option<Section>,
+    current_section_hash: u64,
 }
 
 impl ParserObject<'_> {
@@ -304,6 +338,8 @@ impl ParserObject<'_> {
             pos: 0,
             buf: text.as_bytes(),
             text: text,
+            current_section: None,
+            current_section_hash: 0,
         }
     }
 
@@ -401,13 +437,25 @@ impl ParserObject<'_> {
         }
         end += 1;
         // now the section name is between start and end
-        println!("Section found: {}", &self.text[start..end]);
+        let section_hash = compute_string_hash(&self.buf[start..end]);
+        if self.ini.sections.contains_key(&section_hash) {
+            return Err(self.build_error_message(
+                "A section with the same name already exists !",
+                start,
+                end,
+            ));
+        }
+        self.current_section = Some(Section {
+            name: String::from(&self.text[start..end]),
+            items: HashMap::with_capacity(4),
+        });
+        self.current_section_hash = section_hash;
         Ok(index)
     }
 
     #[inline]
     fn add_value(&mut self, start: usize, end: usize) {
-        println!("Value = {}",&self.text[start..end]);
+        println!("Value = {}", &self.text[start..end]);
     }
 
     #[inline]
@@ -468,11 +516,11 @@ impl ParserObject<'_> {
             && (self.buf[index + 2] == quote_char)
         {
             let next = self.parse_three_quotes_string(index)?;
-            self.add_value(index+3,next-3);
+            self.add_value(index + 3, next - 3);
             index = next;
         } else {
             let next = self.parse_single_quote_string(index)?;
-            self.add_value(index+1,next-1);
+            self.add_value(index + 1, next - 1);
             index = next;
         }
         Ok(index)
@@ -483,27 +531,27 @@ impl ParserObject<'_> {
         // assume that we start with a valid character
         // we should move until the end of the line or until a comment is found
         let start = index;
-        while index<self.buf.len() {
+        while index < self.buf.len() {
             let ch_type = self.get_char_type(index);
             if (ch_type == CharType::NewLine) || (ch_type == CharType::Comment) {
                 break;
             }
-            index+=1;
+            index += 1;
         }
         // trim any extra spaces from the end
-        while (index>start) && (self.get_char_type(index)==CharType::Space) {
-            index-=1;
+        while (index > start) && (self.get_char_type(index) == CharType::Space) {
+            index -= 1;
         }
-        index+=1;
+        index += 1;
         // now we have a value between start and index
         self.add_value(start, index);
         Ok(index)
     }
 
     #[inline]
-    fn parse_key_name(&mut self, index: usize)-> Result<usize, String> {
+    fn parse_key_name(&mut self, index: usize) -> Result<usize, String> {
         let next = self.parse_same_type(index);
-        println!("key={}",&self.text[index..next]);
+        println!("key={}", &self.text[index..next]);
         self.status = Status::ExpectAssign;
         Ok(next)
     }
@@ -573,7 +621,9 @@ impl ParserObject<'_> {
 
 impl Ini {
     pub fn new() -> Ini {
-        Ini {}
+        Ini {
+            sections: HashMap::with_capacity(4),
+        }
     }
     pub fn from<'a>(text: &'a str) -> Result<Ini, String> {
         let mut i = Ini::new();
