f807b4597b311e799b98da0ec52195f4ae68addf
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-11-10 22:46:33 +0200

    Tests for decimal and hexazeciml value

--- src/ini/tests.rs
+++ src/ini/tests.rs
@@ -20,3 +20,20 @@ fn check_bool_value() {
     assert_eq!(sect.get("false_value"),Some(&Value::Bool(false)));
     assert_eq!(sect.get("sone_value"),None);
 }
+
+#[test]
+fn check_int_value() {
+    let i = Ini::from("[Section]\nv1 = 100\nv2 = -100\nv3 = 0\nv4 = 0xFF\nv5=12345678\nv6=0x1122334455667788");
+    assert!(i.is_ok(),"Fail to initialize a simple ini object");
+    let ini = i.unwrap();
+    let sect = ini.get_section("Section");
+    assert!(sect.is_some(),"Unable to found section [Section]");
+    let sect = sect.unwrap();    
+    assert_eq!(sect.get("v1"),Some(&Value::UInt64(100)));
+    assert_eq!(sect.get("v2"),Some(&Value::Int64(-100)));
+    assert_eq!(sect.get("v3"),Some(&Value::UInt64(0)));
+    assert_eq!(sect.get("v4"),Some(&Value::UInt64(0xFF)));
+    assert_eq!(sect.get("v5"),Some(&Value::UInt64(12345678)));
+    assert_eq!(sect.get("v6"),Some(&Value::UInt64(0x1122334455667788)));
+    assert_eq!(sect.get("sone_value"),None);
+}
