d9e1e250434e0579276ed2587e87b4d2b1f32ca6
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-11-12 07:59:58 +0200

    Refactor Value to include a trait to automatically convert data

--- src/ini/entry.rs
+++ src/ini/entry.rs
@@ -1,19 +1,25 @@
 use super::key_value::KeyValue;
-#[derive(Debug)]
-pub struct Entry<'a>
-{
-    pub(super) data: Option<&'a KeyValue>
-}
+use super::value::CreateFromValue;
 
-impl Entry<'_> {    
-    pub fn name(&self)-> &str {
+#[derive(Debug)]
+pub struct Entry<'a> {
+    pub(super) data: Option<&'a KeyValue>,
+}
+impl Entry<'_> {
+    pub fn name(&self) -> &str {
         if self.data.is_some() {
             return self.data.clone().unwrap().name.as_str();
         } else {
             return "";
         }
     }
-    pub fn exists(&self)->bool {
+    pub fn exists(&self) -> bool {
         self.data.is_some()
+    }
+    pub fn or_else<T: CreateFromValue<T>>(&self, default: T) -> T {
+        if self.data.is_none() {
+            return default;
+        }
+        return T::create_from_value(self.data.clone().unwrap().get_value(), default);        
     }
 }
--- src/ini/value.rs
+++ src/ini/value.rs
@@ -8,6 +8,30 @@ pub enum Value {
     Array(Vec<Value>),
 }
 
+pub trait CreateFromValue<T> {
+    fn create_from_value(val: &Value, default: T)->T;
+}
+
+impl CreateFromValue<i32> for i32 {
+    fn create_from_value(val: &Value, default: i32)->i32 {
+        if let Value::UInt64(cval) = val {
+            if (*cval)<=(i32::MAX as u64) {
+                return (*cval) as i32;
+            } else {
+                return default;
+            }
+        }
+        if let Value::Int64(cval) = val {
+            if ((*cval)<=(i32::MAX as i64)) && ((*cval)>=(i32::MIN as i64)) {
+                return (*cval) as i32;
+            } else {
+                return default;
+            }
+        }
+        return default;       
+    }
+}
+
 impl From<String> for Value {
     fn from(value: String) -> Self {
         Self::String(value)
